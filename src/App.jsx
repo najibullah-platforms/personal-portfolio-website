f48ad58315028470d145194dcaee1b3b539bab2f
najibullah-platforms <najibullah.platforms@gmail.com>
2025-08-26 00:33:33 +0500

    Update Navbar component and styles

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './component/navbar/Navbar.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
