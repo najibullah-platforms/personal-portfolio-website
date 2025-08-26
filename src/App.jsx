87051ef280f25abafbdc692c71e3575da9d9845b
najibullah-platforms <najibullah.platforms@gmail.com>
2025-08-26 18:46:30 +0500

    services seetion is completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './component/navbar/Navbar.jsx'
 import Hero from './component/Hero/Hero.jsx'
 import About from './component/About/About.jsx'
+import Services from './component/Services/Services.jsx'
+import MyWork from './component/MyWork/MyWork.jsx'
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
+      <Services />
+      <MyWork/>
     </div>
   )
 }
---
+++ src/component/MyWork/MyWork.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './MyWork.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+const MyWork = () => {
+  return (
+    <div className='mywork'>
+      <div className='mywork-title'>
+        <h1>My Last Work</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default MyWork
