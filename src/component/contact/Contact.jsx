ada9c873b4825e768704ea02d00ef819b4f27fe9
najibullah-platforms <najibullah.platforms@gmail.com>
2025-08-27 14:41:35 +0500

    responsive querys added to all pages

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -5,7 +5,6 @@ import mail_icon from "../../assets/mail_icon.svg";
 import location_icon from "../../assets/location_icon.svg";
 import call_icon from "../../assets/call_icon.svg";
 const Contact = () => {
- 
   const onSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -19,13 +18,13 @@ const Contact = () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json"
+        Accept: "application/json",
       },
-      body: json
+      body: json,
     }).then((res) => res.json());
 
     if (res.success) {
-      console.log("Success", res);
+     alert(res.message)
     }
   };
   return (
@@ -36,29 +35,35 @@ const Contact = () => {
       </div>
       <div className="contact-section">
         <div className="contact-left">
-            <h1>Let's talk</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit et, tenetur labore, iure amet ducimus alias eligendi nesciunt quo dolorem quos perferendis, sit iste. Quod minima expedita accusantium ea.
-            </p>
-            <div className="contact-details">
-                <div className="contact-detail">
-                  <img src={mail_icon} alt="" />   <p>najiullah@gmail.com</p>                  
-                </div>
-                <div className="contact-detail">
-                  <img src={location_icon} alt="" /> <p>033054 030504</p>
-                </div>
-                <div className="contact-detail">
-                  <img src={call_icon} alt="" /> <p>rawilppnid pakistan sadder</p>
-                </div>
+          <h1>Let's talk</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit et,
+            tenetur labore, iure amet ducimus alias eligendi nesciunt quo
+            dolorem quos perferendis, sit iste. Quod minima expedita accusantium
+            ea.
+          </p>
+          <div className="contact-details">
+            <div className="contact-detail">
+              <img src={mail_icon} alt="" /> <p>najiullah@gmail.com</p>
             </div>
+            <div className="contact-detail">
+              <img src={location_icon} alt="" /> <p>033054 030504</p>
+            </div>
+            <div className="contact-detail">
+              <img src={call_icon} alt="" /> <p>rawilppnid pakistan sadder</p>
+            </div>
+          </div>
         </div>
         <form onSubmit={onSubmit} className="contact-right">
-            <label htmlFor="">Your Name</label>
-            <input type="text" placeholder="Enter Your name" name="name" />
-            <label htmlFor="">Email</label>
-            <input type="email" placeholder="Enter Your email" name="email" />
-            <label htmlFor="">Message</label>
-            <textarea name="message" placeholder="Enter Your message"  rows="8"/>
-            <button type="submit" className="contact-submit">Submit Now</button>
+          <label htmlFor="">Your Name</label>
+          <input type="text" placeholder="Enter Your name" name="name" />
+          <label htmlFor="">Email</label>
+          <input type="email" placeholder="Enter Your email" name="email" />
+          <label htmlFor="">Message</label>
+          <textarea name="message" placeholder="Enter Your message" rows="8" />
+          <button type="submit" className="contact-submit">
+            Submit Now
+          </button>
         </form>
       </div>
     </div>
